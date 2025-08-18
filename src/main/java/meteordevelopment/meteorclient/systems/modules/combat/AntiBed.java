4ce4cc73404a919febef6ed43e0ce0dd5da93c42
Wvszt <mateusz.blokesz@op.pl>
2025-08-18 20:31:14 +0200

    Update AntiBed.java

--- src/main/java/meteordevelopment/meteorclient/systems/modules/combat/AntiBed.java
+++ src/main/java/meteordevelopment/meteorclient/systems/modules/combat/AntiBed.java
@@ -58,7 +58,7 @@ public class AntiBed extends Module {
     private boolean breaking;
 
     public AntiBed() {
-        super(Categories.Combat, "anti-bed", "Places string to prevent beds being placed on you.");
+        super(Categories.Combat, "anti-bed", "Kładzie nić (nwm po chuj ale ok).");
     }
 
     @EventHandler
