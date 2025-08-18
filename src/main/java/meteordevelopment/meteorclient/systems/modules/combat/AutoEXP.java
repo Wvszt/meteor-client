37e139b319bb67456c6a687a3f0fe2c8a7cbe73b
Wvszt <mateusz.blokesz@op.pl>
2025-08-18 22:08:58 +0200

    Update AutoEXP.java

--- src/main/java/meteordevelopment/meteorclient/systems/modules/combat/AutoEXP.java
+++ src/main/java/meteordevelopment/meteorclient/systems/modules/combat/AutoEXP.java
@@ -70,7 +70,7 @@ public class AutoEXP extends Module {
     private int repairingI;
 
     public AutoEXP() {
-        super(Categories.Combat, "auto-exp", "Automatically repairs your armor and tools in pvp.");
+        super(Categories.Combat, "auto-exp", "Automatycznie naprawio ci zbroja i itemy jak sie nakurwiosz.");
     }
 
     @Override
