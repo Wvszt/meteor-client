26fab209cc3217c3d2900c2a5f8a25fa33fb6ae6
Wvszt <mateusz.blokesz@op.pl>
2025-08-19 11:46:12 +0200

    Update AutoReconnect.java

--- src/main/java/meteordevelopment/meteorclient/systems/modules/misc/AutoReconnect.java
+++ src/main/java/meteordevelopment/meteorclient/systems/modules/misc/AutoReconnect.java
@@ -41,7 +41,7 @@ public class AutoReconnect extends Module {
     public Pair<ServerAddress, ServerInfo> lastServerConnection;
 
     public AutoReconnect() {
-        super(Categories.Misc, "auto-reconnect", "Automatically reconnects when disconnected from a server.");
+        super(Categories.Misc, "auto-reconnect", "Automatycznie łączy cie z serwerem.");
         MeteorClient.EVENT_BUS.subscribe(new StaticListener());
     }
 
